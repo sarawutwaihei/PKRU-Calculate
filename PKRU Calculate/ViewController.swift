a6a044e0a44a90a41c6e537eba6299671c89ac12
Com03 <com03@Com03s-iMac.local>
2017-07-25 16:01:54 +0700

    TEST

--- PKRU Calculate/ViewController.swift
+++ PKRU Calculate/ViewController.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var andwerOutlet: UILabel!
+    
+    
+    @IBOutlet weak var Number1Outlet: UITextField!
+    
+    
+    @IBOutlet weak var Number2Outlet: UITextField!
+    
+    
+ 
+    @IBAction func calculateButton(sender: AnyObject) {
+        
+        //Get Value From TextField
+        let strNumber1 = Number1Outlet.text
+        
+        
+        let strNumber2 = Number2Outlet.text
+        
+        print("strNumber1 = \(strNumber1)")
+        print("strNumber2 = \(strNumber2)")
+        
+        let intAnswer = Int(strNumber1) + Int(strNumber2)
+        
+        andwerOutlet.text = String()
+        
+        
+        
+    } // calculateButton
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +50,8 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }  //Second Method
 
 
-}
+}  //Main Class
 
