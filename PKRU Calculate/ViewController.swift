a7f448f927e6f665fc21855ae5b8a932c939007a
Com03 <com03@Com03s-iMac.local>
2017-07-25 14:29:59 +0700

    Initial Commit

---
+++ PKRU Calculate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PKRU Calculate
+//
+//  Created by Com03 on 7/25/2560 BE.
+//  Copyright (c) 2560 Com03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PKRU CalculateTests/PKRU_CalculateTests.swift
@@ -0,0 +1,36 @@
+//
+//  PKRU_CalculateTests.swift
+//  PKRU CalculateTests
+//
+//  Created by Com03 on 7/25/2560 BE.
+//  Copyright (c) 2560 Com03. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PKRU_CalculateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
